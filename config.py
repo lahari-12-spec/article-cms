657b6280c0571b8061bc519f93f0dbfa0970c9ae
lahari-12-spec <laharireddy1222@gmail.com>
2026-06-04 19:02:14 +0530

    Use tenant specific authority

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    AUTHORITY = "https://login.microsoftonline.com/f958e84a-92b8-439f-a62d-4f45996b6d07"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
     CLIENT_ID = os.environ.get('CLIENT_ID')
